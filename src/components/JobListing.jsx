2cb0cf619c3f72f87d932b0e00636a2e115f5e25
BenITRD <m.akif.azmi@gmail.com>
2024-05-05 21:58:19 +0800

    restructure the component

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import jobs from '../jobs.json'
 
 const JobListing = () => {
-    console.log(jobs)
+    const recentJobs = jobs.slice(0, 3)
     return (
         <section className='bg-blue-50 px-4 py-10'>
             <div className='container-xl lg:container m-auto'>
@@ -10,9 +10,8 @@ const JobListing = () => {
                     Browse Jobs
                 </h2>
                 {/* Job Listing */}
-                
-                    <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-                    {jobs.map((job) => (
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+                    {recentJobs.map((job) => (
                         <div className='bg-white rounded-xl shadow-md relative'>
                             <div className='p-4'>
                                 <div className='mb-6'>
@@ -36,9 +35,9 @@ const JobListing = () => {
                                 </div>
                             </div>
                         </div>
-                        ))}
-                    </div>
-                
+                    ))}
+                </div>
+
             </div>
         </section>
     )
